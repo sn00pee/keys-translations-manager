0fc32134e8c411d326c0053f073cb1194fc78556
Unknown <contact@ericseigers.com>
2018-10-07 17:27:33 +0900

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,8 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'semi': 0
+    'semi': 0,
+    'comma-dangle': 0
   },
   parserOptions: {
     parser: 'babel-eslint',
