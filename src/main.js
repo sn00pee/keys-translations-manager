41842ba35dfdb132ab59578277784a3c45722618
Unknown <contact@ericseigers.com>
2018-10-07 16:05:04 +0900

    add home

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
-import App from './App.vue'
 import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' // Vuesax styles
+import App from './App.vue'
 
-import 'vuesax/dist/vuesax.css' //Vuesax styles
 // import router from './router';
 // import store from './store';
 
---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="home">
+    this is Home
+    this will list an overview of all projects
+  </div>
+</template>
+<script>
+export default {
+  name: "",
+  data: () => ({
+
+  })
+}
+</script>
+<style lang="scss" scoped>
+</style>
