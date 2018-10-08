16e600654bf0a01af14d09063050180dc0aebe24
Unknown <contact@ericseigers.com>
2018-10-08 16:09:18 +0900

    add material icons css

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuesax from 'vuesax'
 import 'vuesax/dist/vuesax.css' //Vuesax styles
+import 'material-icons/iconfont/material-icons.css'
 import App from './App.vue'
 import router from './router';
 import store from './store';
