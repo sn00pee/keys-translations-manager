a6c8186030a86abe4544298acc005bdd5a5d47d9
Unknown <contact@ericseigers.com>
2018-10-08 16:08:36 +0900

    import locales and projects to store from config

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,17 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from 'vue'
+import Vuex from 'vuex'
 
-Vue.use(Vuex);
-
+Vue.use(Vuex)
+import config from '../../ktm.config.js'
 export default new Vuex.Store({
   state: {
-
+    locales: config.locales,
+    projects: config.projects
+  },
+  getters: {
+    getProject: (state) => (id) => {
+      return state.projects.find(project => project.id === id)
+    }
   },
   mutations: {
 
@@ -13,4 +19,4 @@ export default new Vuex.Store({
   actions: {
 
   },
-});
+})
